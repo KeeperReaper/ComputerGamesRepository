49a906e1f0f6ec3f936ac1761891056b5ebd4a09
keeperreaper <keeperreaper09@gmail.com>
2017-11-26 18:55:52 +0100

    11/26/2017: 	Inverted Paddles. Player 1 now controls with keyboard and Player 2 with mouse

--- Assets/Resources/Scripts/Player2Paddle.cs
+++ Assets/Resources/Scripts/Player2Paddle.cs
@@ -2,28 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player2Paddle : MonoBehaviour
-{
+public class Player2Paddle : MonoBehaviour {
 
     Vector3 paddlePosition;
     // Use this for initialization
-    void Start()
-    {
+    void Start () {
         paddlePosition = transform.position;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-        {
-            paddlePosition.y -= 10f;
-        }
-        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-        {
-            paddlePosition.y += 10f;
-        }
-        paddlePosition.y = Mathf.Clamp(paddlePosition.y, 50f, 555f);
+	
+	// Update is called once per frame
+	void Update () {
+        paddlePosition.y = Mathf.Clamp(Input.mousePosition.y, 50f, 555f);
         transform.position = paddlePosition;
     }
 }
